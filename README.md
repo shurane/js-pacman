4fc67bb6d90ffe0f7742edab82369d9853c943ed
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2015-12-16 19:59:53 -0500

    event.preventDefault()

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 pacman in Javascript
+
+Jesse and I worked on this in Javascript. Pretty rad stuff.
--- script.js
+++ script.js
@@ -3,20 +3,23 @@ var pacman = document.getElementById("pacman"),
 var position = {x: 0, y: 0};
 
 window.addEventListener("keydown", function (event) {
-  event.preventDefault();
   if (event.which === 39){
+    event.preventDefault();
     pacmanGfx.className = "right";
     position.x += 100;
   }
   else if (event.which === 37){
+    event.preventDefault();
     pacmanGfx.className = "left";
     position.x -= 100;
   }
   else if (event.which === 38){
+    event.preventDefault();
     pacmanGfx.className = "up";
     position.y -= 100;
   }
   else if (event.which === 40){
+    event.preventDefault();
     pacmanGfx.className = "down";
     position.y += 100;
   }
